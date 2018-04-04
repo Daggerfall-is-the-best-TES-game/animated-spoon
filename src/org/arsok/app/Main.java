713ae82d6d1d9f70bb17239cb4fa4370d072d669
SirMathhman <minecraftmathhman@gmail.com>
2018-04-04 08:22:25 -0400

    Added display code to Main

--- src/org/arsok/app/Main.java
+++ src/org/arsok/app/Main.java
@@ -1,4 +1,25 @@
 package org.arsok.app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+    private final URL displayURL = getClass().getResource("/Display.fxml");
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(displayURL);
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
