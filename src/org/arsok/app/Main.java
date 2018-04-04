2abefb2406069085438aaf884708d62c82fbc21c
SirMathhman <minecraftmathhman@gmail.com>
2018-04-04 08:45:04 -0400

    Added logging code.

--- src/org/arsok/app/Main.java
+++ src/org/arsok/app/Main.java
@@ -6,14 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends Application {
     public static Main instance;
     private final URL displayURL = getClass().getResource("/Display.fxml");
     private final ExecutorService service = Executors.newCachedThreadPool();
+    private final Logger logger = Logger.getLogger("Application");
 
     public static void main(String[] args) {
         launch(args);
@@ -23,26 +29,56 @@ public class Main extends Application {
         return service;
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        instance = this;
+    public void log(Level level, String message, Exception e) {
+        StringBuilder builder = new StringBuilder();
 
-        Parent parent = FXMLLoader.load(displayURL);
-        Scene scene = new Scene(parent);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        if (message != null) {
+            builder.append(message);
+        }
 
-        //TODO: handle exceptions
+        if (message != null && e != null) {
+            builder.append("\n");
+        }
+
+        if (e != null) {
+            StringWriter writer = new StringWriter();
+            e.printStackTrace(new PrintWriter(writer));
+            builder.append(writer.toString());
+        }
+
+        logger.log(level, builder.toString());
     }
 
     @Override
-    public void stop(){
+    public void start(Stage primaryStage) {
+        instance = this;
+
+        log(Level.INFO, "Starting application", null);
+
+        try {
+            Parent parent = FXMLLoader.load(displayURL);
+            Scene scene = new Scene(parent);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            log(Level.SEVERE, "Failed to load display", e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        log(Level.INFO, "Stopping application", null);
+
         service.shutdown();
 
-        if(!service.isShutdown()){
+        if (!service.isShutdown()) {
+            log(Level.INFO, "Executor service was not shutdown, firmly shutting down now!", null);
+
             try {
                 Thread.sleep(1000);
                 service.shutdownNow();
+
+                log(Level.INFO, "Executor service was shutdown successfully", null);
             } catch (InterruptedException e) {
                 System.exit(-1);
             }
