a6d0c910aca29d068d86ff3f37917e03119fbe1e
Daggerfall-Is-The-Best-TES-Game <Algoalt904@gmail.com>
2018-05-02 19:15:17 -0400

    finished implementation of formula for deflection of light rays. TODO: testing that formula

--- src/main/java/org/arsok/app/RayTrace.java
+++ src/main/java/org/arsok/app/RayTrace.java
@@ -47,25 +47,60 @@ public class RayTrace {
         //Project Plan V2 with https://arxiv.org/pdf/0804.4112.pdf
         double rSubZero = 1e5; //radius- 100 kilometers away from black hole
         double theta = 0.5; //radians- temp value. angle between path of photon and radius vector of black hole
-        double rSubg = 2 * blackHole.getMass(); //Schwarzschild radius in meters
-        double n = 1; //number of dimensions including time - 3
-        double qSubStar = Math.pow(2 / (n + 2), 1 / n); //position that gives minimum of lSubmax
-        double lambda = rSubZero * Math.sin(theta); //impact parameter
-        double l = lambda / rSubg;
+
 
         //bending angle of light ray. This is a big deal
         private double phi(double rSubZero, double theta) {
+            double rSubg = 2 * blackHole.getMass(); //Schwarzschild radius in meters
+            double n = 1; //number of dimensions including time - 3
+            double qSubStar = Math.pow(2 / (n + 2), 1 / n); //position that gives minimum of lSubmax
+            double lambda = rSubZero * Math.sin(theta); //impact parameter
+            double l = lambda / rSubg;
 
-            double qSubZero = q(rSubZero); //dimensionless quantity
+            double qSubZero = q(rSubZero, rSubg); //dimensionless quantity
+            double pSubZero = qSubZero / qSubStar;
 
+            double nu = l / (1.0 / ((rSubg / rSubZero) * Math.sqrt(1 - Math.pow((rSubg / rSubZero), n))));
 
-            double P = Math.pow(p(rSubZero), n); //fixed
-            double B = Math.pow(nu(rSubZero), 2); //fixed
+            //calculation of deltaPhiHat
+            double kSubPhi = 0.47; //constant parameter for 4 dimensionional spacetime
+            double phiSubPhi = Math.PI / 45;
 
-            return 0; //temp
+            if (qSubZero <= qSubStar) { // Type I rays bending angle. formula (50) in paper
+                double P = Math.pow(p(rSubZero, qSubStar, rSubg), n); //fixed
+                double B = Math.pow(nu(rSubZero, rSubg, n, l), 2); //fixed
+                double a = (1 - B) * (1 - (2 * P) / (n + 2));
+                double b = B * (1 - P);
+                double c = -B * (1 - (n + 1) * P);
+                double A = 1 - (2 * P) / (n + 2) + (n * (n + 1) * B * P) / (n + 2);
+
+                double D = a * c - Math.pow(b, 2);
+                //calculation of phiHat
+                double phiHat;
+                if (c < 0) {
+                    phiHat = (nu / Math.sqrt(-c)) * Math.asin(((Math.sqrt(-c) * B) / D) * (n * P * Math.sqrt(a) - (1 - P) * Math.sqrt(A)));
+                } else if (c == 0) {
+                    phiHat = (nu / b) * (Math.sqrt(a + 2 * b) - Math.sqrt(a));
+                } else { //c > 0
+                    phiHat = (nu / Math.sqrt(c)) * Math.log((n * B * P + Math.sqrt(c * A)) / (b + Math.sqrt(a * c)));
+                }
+                double xSubPhi = Math.sqrt(2) * (Math.cos((Math.PI / 4) + phiSubPhi) * pSubZero + Math.sin((Math.PI / 4) + phiSubPhi) * nu);
+                double deltaPhiHat = -kSubPhi * (Math.sqrt(1 - Math.pow(xSubPhi - 1, 2)) - 1) * heavisideStepFunction(xSubPhi - 1);
+                return phiHat + deltaPhiHat; //phi, the bending angle
+            } else { //qSubZero > qSubStar. Type II rays bending angle. formula (61) in paper
+                //calculation of phiHatSubStar. needed for when P = 1. approximation. formula (47). replace with formula 46 if not good enough
+                double lSubStar = Math.sqrt((n + 2) / n) * Math.pow((n + 2) / 2, 1 / n);
+                double mu = l / lSubStar;
+                double phiHatSubStar = (1.0 / Math.sqrt(n)) * Math.log((mu * Math.sqrt(n + 2) + Math.sqrt((n + 2) * Math.pow(mu, 2) + 1 - Math.pow(mu, 2))) / (Math.sqrt(1 - Math.pow(mu, 2))));
+                double xSubPhiStar = Math.sqrt(2) * (Math.cos((Math.PI / 4) + phiSubPhi) * 1 + Math.sin((Math.PI / 4) + phiSubPhi) * nu); //like xSubPhi, but pSubZero = 1
+                double deltaPhiHatSubStar = -kSubPhi * (Math.sqrt(1 - Math.pow(xSubPhiStar - 1, 2)) - 1) * heavisideStepFunction(xSubPhiStar - 1);
+                double z = mu * Math.sqrt(n + 2) * (pSubZero - 1);
+                double phiHatSubII = (1 / Math.sqrt(n)) * Math.log((z + Math.sqrt(Math.pow(z, 2) + 1 - Math.pow(mu, 2))) / Math.sqrt(1 - Math.pow(mu, 2)));
+                return phiHatSubStar + deltaPhiHatSubStar + phiHatSubII;
+            }
         }
 
-        private double q(double radius) { //dimensionless radius
+        private double q(double radius, double rSubg) { //dimensionless radius
             return rSubg / radius;
         }
 
@@ -73,16 +108,23 @@ public class RayTrace {
             return 1 - rSubZero / radius;
         }
 
-        private double p(double radius) {
-            return q(radius) / qSubStar;
+        private double p(double radius, double qSubStar, double rSubg) {
+            return q(radius, rSubg) / qSubStar;
         }
 
-        private double lSubmax(double radius) {
-            return 1.0 / (q(radius) * Math.sqrt(1 - Math.pow(q(radius), n)));
+        private double lSubmax(double radius, double rSubg, double n) {
+            return 1.0 / (q(radius, rSubg) * Math.sqrt(1 - Math.pow(q(radius, rSubg), n)));
         }
 
-        private double nu(double radius) {
-            return l / lSubmax(radius);
+        private double nu(double radius, double rSubg, double n, double l) {
+            return l / lSubmax(radius, rSubg, n);
+        }
+
+        private double heavisideStepFunction(double n) {
+            if (n <= 0) {
+                return 0;
+            }
+            return 1;
         }
 
 
