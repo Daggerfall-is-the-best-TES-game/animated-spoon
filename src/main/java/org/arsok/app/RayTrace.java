fddeca9806b19de9dcce95f9aeb18b19273ceb71
Daggerfall-is-the-best-TES-game <33900090+Daggerfall-is-the-best-TES-game@users.noreply.github.com>
2018-05-21 08:00:42 -0400

    finished bending angle formula

--- src/main/java/org/arsok/app/RayTrace.java
+++ src/main/java/org/arsok/app/RayTrace.java
@@ -128,20 +128,51 @@ public class RayTrace {
         //The bending angle is the angle between the initial trajectory of the light ray and the radius vector(tail at black hole, head at photon)
         //Note that this means that vector trajectories
         //using formulas 27 and 23 from https://arxiv.org/pdf/gr-qc/0611086.pdf
+        //valid for 0 < theta < 90 degrees
         private double phi(double rSubZero, double theta) {
+            double G = 6.67408e-11; //https://en.wikipedia.org/wiki/Gravitational_constant
+            double c = 299792458; //https://en.wikipedia.org/wiki/Speed_of_light
+            double mDot = G * blackHole.getMass() / (c * c); //https://en.wikipedia.org/wiki/Schwarzschild_radius
+            double b = Math.abs(rSubZero * Math.sin(theta)); //impact parameter
+            double bCritical = 3 * Math.sqrt(3) * mDot; //critical impact parameter
 
+            if (b < bCritical) {
+                return Double.NaN;
+            }
+
+            double bPrime = 1 - (bCritical / b); //invariant paramter
+
+
+            System.out.format("b: %.2f", bPrime);
+            if (bPrime < 0.4705) { //light is close to black hole. strong lensing equation
+                double bendingAngle = 0;
+                double lambdaSubZero = 216 * (7 - 4 * Math.sqrt(3));
+                double[] sigmaCoeff = {1, 5.0 / 18, 205.0 / 1296, 68145.0 / 629856};
+                double[] rhoCoeff = {-Math.PI, (-17 + 4 * Math.sqrt(3)) / 18, (-879 + 236 * Math.sqrt(3)) / 1269, (-321590 + 90588 * Math.sqrt(3)) / 629857};
+                for (int i = 0; i < sigmaCoeff.length; i++) {
+                    bendingAngle += sigmaCoeff[i] * Math.pow(bPrime, i) * Math.log(lambdaSubZero / bPrime) + rhoCoeff[i] * Math.pow(bPrime, i);
+                }
+                return bendingAngle;
+            } else { //light is far from black hole. weak lensing equation
+                double bendingAngle = 0;
+                double[] coefficients = {4 / (3 * Math.sqrt(3)), 5 * Math.PI / 36, 128 / (243 * Math.sqrt(3)), 385 * Math.PI / 5184, 3584 / (10935 * Math.sqrt(3))};
+                for (int i = 0; i < coefficients.length; i++) {
+                    bendingAngle += coefficients[i] * Math.pow((1 - bPrime), i + 1);
+                }
+                return bendingAngle;
+            }
         }
 
 
         @Override
         public void run() {
             //Project Plan V2 with https://arxiv.org/pdf/0804.4112.pdf
-            double rSubZero = 2e12; //radius
-            double theta = 2; //radians- temp value. angle between path of photon and radius vector of black hole
+            double rSubZero = 3 * Math.sqrt(3) * 6.67408e-11 * blackHole.getMass() / (299792458.0 * 299792458) * 1.5; //radius
             //testing the output of the phi function, which is the bending angle
             for (double i = 0; i < Math.PI * 2; i += Math.PI / 100) {
                 System.out.format("emission angle (degrees): %.1f Phi (degrees): %.1f%n", Math.toDegrees(i), Math.toDegrees(phi(rSubZero, i)));
             }
+
             /*
             for (int i = 0; i < image.getWidth(); i++) {
                 for (int j = 0; j < image.getHeight(); j++) {
