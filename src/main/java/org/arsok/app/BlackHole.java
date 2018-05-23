4491f089d9778540b9461cccd24604b417825c3b
SirMathhman <minecraftmathhman@gmail.com>
2018-05-23 08:38:52 -0400

    Removing a todo.

--- src/main/java/org/arsok/app/BlackHole.java
+++ src/main/java/org/arsok/app/BlackHole.java
@@ -1,16 +1,10 @@
 package org.arsok.app;
 
 public class BlackHole {
-    //TODO: fields
     private double mass; //in kilograms
 
     public BlackHole(double mass) {
         this.mass = mass;
-
-    }
-
-    public BlackHole() {
-        this.mass = 1e31; //default value of mass of black hole in Kilograms
 
     }
 
